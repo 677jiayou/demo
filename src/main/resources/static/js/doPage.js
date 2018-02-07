f19399154419577f676e32b8180b7c002a8bcf55
677jiayou <444718843@qq.com>
2018-02-07 13:19:14 +0800

    datatables，牛！

--- src/main/resources/static/js/doPage.js
+++ src/main/resources/static/js/doPage.js
@@ -2,20 +2,20 @@
 $().ready(function () {
     $('.dataTables-example').dataTable();
 
-    var oTable = $('#editable').dataTable();
+    // var oTable = $('#editable').dataTable();
 
-    oTable.$('td').editable('../example_ajax.php', {
-        "callback": function (sValue, y) {
-            var aPos = oTable.fnGetPosition(this);
-            oTable.fnUpdate(sValue, aPos[0], aPos[1]);
-        },
-        "submitdata": function (value, settings) {
-            return {
-                "row_id": this.parentNode.getAttribute('id'),
-                "column": oTable.fnGetPosition(this)[2]
-            };
-        },
-        "width": "90%",
-        "height": "100%"
-    });
+    // oTable.$('td').editable('../example_ajax.php', {
+    //     "callback": function (sValue, y) {
+    //         var aPos = oTable.fnGetPosition(this);
+    //         oTable.fnUpdate(sValue, aPos[0], aPos[1]);
+    //     },
+    //     "submitdata": function (value, settings) {
+    //         return {
+    //             "row_id": this.parentNode.getAttribute('id'),
+    //             "column": oTable.fnGetPosition(this)[2]
+    //         };
+    //     },
+    //     "width": "90%",
+    //     "height": "100%"
+    // });
 });
